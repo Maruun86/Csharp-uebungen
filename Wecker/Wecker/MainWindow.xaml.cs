78859a81b4783aa8c7bae843bbcfcd7d6f3e8eb2
Maruun86 <pierrea@web.de>
2022-06-16 14:01:00 +0200

    Wecker

---
+++ Wecker/Wecker/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wecker
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Termin> ListTermin = new List<Termin>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Add(object sender, RoutedEventArgs e)
+        {
+            string title = textBoxTitle.Text;
+
+            int hours;
+            bool result = int.TryParse(textBoxHours.Text, out hours);
+            int minutes;
+            result =int.TryParse(textBoxMinutes.Text, out minutes);
+            
+            if ( hours > 23 | minutes > 59 ) { result = false; }
+            if (result)
+            {
+                CreateTermin(title, hours, minutes);
+            }
+            else
+            {
+                MessageBox.Show("Es gab ein Fehler, überprüfen Sie ihre Angaben");
+            }
+     
+        }
+        private void CreateTermin(string title, int hours, int minutes)
+        {
+            Termin newTermin = new Termin(title, hours, minutes);
+            ListTermin.Add(newTermin);
+            ListBoxTermine.Items.Add(newTermin.ToString());
+
+        }
+
+        private void Button_Remove(object sender, RoutedEventArgs e)
+        {
+            object item = ListBoxTermine.SelectedItem;
+            ListBoxTermine.Items.Remove(item);
+
+        }
+    }
+}
---
+++ Wecker/Wecker/Termin.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wecker
+{
+    class Termin
+    {
+        string title;
+        int hours;
+        int minutes;
+
+        public Termin (String title, int hours, int minutes)
+        {
+            this.title = title;
+            this.hours = hours;
+            this.minutes = minutes;
+        }
+        //---Getter/Setter
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }  
+        }
+        public int Hours
+        {
+            get { return hours; }
+            set { hours = value; }
+        }
+        public int Minutes
+        {
+            get { return minutes; }
+            set { minutes = value; }
+        }
+
+        //-------------------
+        public override string ToString()
+        {
+            string s = this.Title + "-" + this.Hours.ToString() + ":" + this.Minutes.ToString();
+            return s;
+        }
+     
+    }
+}
