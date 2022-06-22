75bb77f02a3733c9c51d158d999b790694bb49ca
Maruun86 <pierrea@web.de>
2022-06-22 14:08:50 +0200

    Asteroid Teil 2

--- Asteroid-Übung/Asteroid-Übung/GameObject.cs
+++ Asteroid-Übung/Asteroid-Übung/GameObject.cs
@@ -1,22 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Drawing;
-using System.Text;
 using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Shapes;
-using System.Windows.Threading;
 
 namespace Asteroid_Übung
 {
-    class GameObject  //TODO : abstract
+    abstract class GameObject
     {
         //vx und vy sind die Geschwindigkeiten in eine Richtung
         double vx;
         double vy;
         double x;
         double y;
-        int size;
         bool periodischeFortsetzung = true;
 
         public GameObject(double x, double y)
@@ -24,39 +20,37 @@ namespace Asteroid_Übung
             this.x = x;
             this.y = y;
         }
+
         public GameObject(double x, double y, double vx, double vy)
         {
             this.x = x;
-            this.y = y; 
+            this.y = y;
             this.vx = vx;
             this.vy = vy;
         }
-        public virtual void DrawSelf(Canvas zeichenfläche)
-        {
-            //leer
-        }
+
+        public abstract void DrawSelf(Canvas zeichenfläche);
+
         public double X
-        { get { return x; } set { x = value; } }  
-        
+        { get { return x; } set { x = value; } }
         public double Y
         { get { return y; } set { y = value; } }
-
-        public int Size
-        {
-            get { return size; }
-            set { size = value; }
-        }
+        public double VX
+        { get { return vx; } set { vx = value; } }
+        public double VY
+        { get { return vy; } set { vy = value; } }
 
         public void Animate(TimeSpan interval, Canvas zeichenfläche)
         {
             x += vx * interval.TotalSeconds;
             y += vy * interval.TotalSeconds;
 
+            //Verlassen des Spielfelds bringt das Objekt wieder auf die gegenüberliegende Seite.
             if (periodischeFortsetzung)
             {
                 if (x > zeichenfläche.ActualWidth)
                 {
-                    x = 0 - Size;
+                    x = 0;
                 }
                 else if (x < 0.0)
                 {
@@ -64,58 +58,132 @@ namespace Asteroid_Übung
                 }
                 if (y > zeichenfläche.ActualHeight)
                 {
-                    y = 0 - Size;
+                    y = 0;
                 }
                 else if (y < 0.0)
                 {
-                    y = zeichenfläche .ActualHeight;
+                    y = zeichenfläche.ActualHeight;
                 }
             }
-            
 
         }
     }
     class Asteroid : GameObject
     {
         static Random zufall = new Random();
-
-        public Asteroid (Canvas zeichenfläche)
-            : base(zufall.NextDouble() * zeichenfläche.ActualWidth, 
+        Polygon umriss = new Polygon();
+        double size;
+        public Asteroid(Canvas zeichenfläche, double size = 16.0)
+            : base(zufall.NextDouble() * zeichenfläche.ActualWidth,
                   zufall.NextDouble() * zeichenfläche.ActualHeight,
-                  (zufall.NextDouble() - 0.5) * 100.0, 
-                  (zufall.NextDouble() -0.5) * 100.0)
+                  (zufall.NextDouble() - 0.5) * 100.0,
+                  (zufall.NextDouble() - 0.5) * 100.0)
         {
-            //leer
+            this.size = size;
+
+            //Einmal wird die Form erstellt
+            for (int i = 0; i < 20; i++)
+            {
+                double alpha = 2.0 * Math.PI / 20 * i;
+                double radius = size + 4.0 * zufall.NextDouble();
+                umriss.Points.Add(new System.Windows.Point(radius * Math.Cos(alpha), radius * Math.Sin(alpha)));
+            }
+            umriss.Fill = Brushes.Gray;
         }
 
         public override void DrawSelf(Canvas zeichenfläche)
         {
-            Polygon poly = new Polygon();
 
-            for (int i = 0; i < 20; i++)
-            {
-                double alpha = 2.0 * Math.PI / 20 * i;
-                double radius = 8.0 + 4.0 *zufall.NextDouble();
-                poly.Points.Add(new System.Windows.Point(radius * Math.Cos(alpha), radius * Math.Sin(alpha)));
-            }
-
-            poly.Fill = Brushes.Gray;
-            zeichenfläche.Children.Add(poly);
-            Canvas.SetLeft(poly, X);
-            Canvas.SetTop(poly, Y);
+            zeichenfläche.Children.Add(umriss);
+            Canvas.SetLeft(umriss, X);
+            Canvas.SetTop(umriss, Y);
+        }
+        public bool containsPoint(double x, double y)
+        {
+            return umriss.RenderedGeometry.FillContains(new System.Windows.Point(x - X, y - Y));
         }
 
-
+        public double Size
+        {
+         get { return size; }
+        }
     }
-    /*
+
     class Ship : GameObject
     {
+        Polygon umriss = new Polygon();
+        List<Photonentorpedo> photonentorpedoes = new List<Photonentorpedo>();
 
+        public Ship(Canvas zeichenfläche)
+            : base(0.5 * zeichenfläche.ActualWidth, 0.5 * zeichenfläche.ActualHeight, 5.0, 1.0)
+        {
+            umriss.Points.Add(new System.Windows.Point(0.0, -15.0));
+            umriss.Points.Add(new System.Windows.Point(10.0, 12.0));
+            umriss.Points.Add(new System.Windows.Point(-10.0, 12.0));
+            umriss.Fill = Brushes.Blue;
+        }
+
+        public override void DrawSelf(Canvas zeichenfläche)
+        {
+            double winkelInGrad = Math.Atan2(VY, VX) * 180.0 / Math.PI + 90;
+
+            umriss.RenderTransform = new RotateTransform(winkelInGrad);
+            zeichenfläche.Children.Add(umriss);
+            Canvas.SetLeft(umriss, X);
+            Canvas.SetTop(umriss, Y);
+        }
+
+        public void BiegeAb(bool nachLinks)
+        {
+            double winkel = (nachLinks ? -5.0 : 5.0) * Math.PI / 180.0;
+
+            double vxNew = Math.Cos(winkel) * VX - Math.Sin(winkel) * VY;
+            VY = Math.Sin(winkel) * VX + Math.Cos(winkel) * VY;
+            VX = vxNew;
+        }
+
+        public void Beschleunige(bool beschleunige)
+        {
+            double faktor = beschleunige ? 1.1 : 0.9;
+
+            VX *= faktor;
+            VY *= faktor;
+
+        }
+        public Photonentorpedo Schuss()
+        {
+            Photonentorpedo newTorpedo = new Photonentorpedo(this);
+            return newTorpedo;
+
+        }
     }
-
     class Photonentorpedo : GameObject
     {
+        Polygon umriss = new Polygon();
+        int lifetime = 2000;
 
+        public Photonentorpedo(Ship ship)
+            : base(ship.X, ship.Y, 1.5 * ship.VX, 1.5 * ship.VY)
+        {
+            umriss.Points.Add(new System.Windows.Point(-2.0, -2.0));
+            umriss.Points.Add(new System.Windows.Point(2.0, -2.0));
+            umriss.Points.Add(new System.Windows.Point(-2.0, 2.0));
+            umriss.Points.Add(new System.Windows.Point(2.0, 2.0));
+            umriss.Fill = Brushes.Red;
+        }
+
+        public int Lifetime
+        { get { return lifetime; } }
+
+        public override void DrawSelf(Canvas zeichenfläche)
+        {
+            double winkelInGrad = Math.Atan2(VY, VX) * 180.0 / Math.PI + 90;
+            umriss.RenderTransform = new RotateTransform(winkelInGrad);
+            zeichenfläche.Children.Add(umriss);
+            Canvas.SetLeft(umriss, X);
+            Canvas.SetTop(umriss, Y);
+            this.lifetime -= 100;
+        }
     }
-    */
+
 }
--- Asteroid-Übung/Asteroid-Übung/MainWindow.xaml.cs
+++ Asteroid-Übung/Asteroid-Übung/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Windows.Threading;
 
 namespace Asteroid_Übung
@@ -21,41 +11,137 @@ namespace Asteroid_Übung
     /// </summary>
     public partial class MainWindow : Window
     {
-        List<Asteroid> asteroids = new List<Asteroid>();
+        List<GameObject> gameObjects = new List<GameObject>();
         DispatcherTimer timer = new DispatcherTimer();
+        Ship player;
 
         public MainWindow()
         {
             InitializeComponent();
             timer.Interval = TimeSpan.FromSeconds(0.1);
             timer.Tick += GameLoop;
-            timer.Start();
-
         }
 
         void GameLoop(object sender, EventArgs e)
         {
-            for (int i = 0; i < asteroids.Count; i++)
-            {
-                asteroids[i].Animate(timer.Interval,zeichenfläche); 
-            }  
-            zeichenfläche.Children.Clear();
+            List<GameObject> newGameObjects = gameObjects;
 
-            for (int i = 0; i < asteroids.Count; i++)
+            for (int i = 0; i < gameObjects.Count; i++)
             {
-                asteroids[i].DrawSelf(zeichenfläche);
+                gameObjects[i].Animate(timer.Interval, zeichenfläche);
+
+                //Lifetime check für Torpedos und löschen
+                if (gameObjects[i].GetType() == typeof(Photonentorpedo))
+                {
+                    Photonentorpedo torp = (Photonentorpedo)newGameObjects[i];
+                    if (torp.Lifetime <= 0)
+                    {
+                        newGameObjects.Remove(newGameObjects[i]);
+                    }
+                }
             }
+            gameObjects = newGameObjects;
+            newGameObjects = this.CheckCollision();
+            zeichenfläche.Children.Clear();
+            //alte Liste mit Änderungen der neuen Liste überschrieben
+
+           
+            gameObjects.ForEach(x => x.DrawSelf(zeichenfläche));
         }
+        private List<GameObject> CheckCollision()
+        {
+           List<Asteroid> asteroids = new List<Asteroid>();
+           List<Photonentorpedo> photonentorpedoes = new List<Photonentorpedo>();
+            List<GameObject> checkGameObjects = gameObjects;
+            //Vorbereitung für Check Split Einträge
+            foreach (GameObject a in gameObjects)
+            {
+                if (a.GetType() == typeof(Asteroid))
+                {
+                    asteroids.Add((Asteroid)a);
+                }
+                else if (a.GetType() == typeof(Photonentorpedo))
+                {
+                    photonentorpedoes.Add((Photonentorpedo)a);
+                }
+                
+            }
+            //finde Einträge mit Collision und
+            foreach (Asteroid a in asteroids)
+            {
+                foreach (Photonentorpedo p in photonentorpedoes)
+                {
+                    if (a.containsPoint(p.X, p.Y))
+                    {
+                        checkGameObjects.Remove(a);
+                        checkGameObjects.Remove(p);
+                        //a wird in 2 kleinere aufgeteilt
+                        checkGameObjects.Add(DestroyAsteroid(a));
+                        checkGameObjects.Add(DestroyAsteroid(a));
+
+                    }
+                    
+                }
+
+            }
+            return checkGameObjects;
+        }
+        private Asteroid DestroyAsteroid(Asteroid a)
+        {
+            Asteroid ast = new Asteroid(zeichenfläche,a.Size / 2);
+            ast.X = a.X;
+            ast.Y = a.Y;
+
+            return ast;
+        }
+
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            player = new Ship(zeichenfläche);
+            gameObjects.Add(player);
+
             for (int i = 0; i < 10; i++)
             {
                 Asteroid ast = new Asteroid(zeichenfläche);
-                asteroids.Add(ast);
+                gameObjects.Add(ast);
             }
 
+            start_button.Visibility = Visibility.Hidden;
+            timer.Start();
 
+        }
 
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (timer.IsEnabled)
+            {
+                switch (e.Key)
+                {
+                    case Key.Left:
+                    case Key.A:
+                        player.BiegeAb(true);
+                        break;
+
+                    case Key.Right:
+                    case Key.D:
+                        player.BiegeAb(false);
+                        break;
+
+                    case Key.Up:
+                    case Key.W:
+                        player.Beschleunige(true);
+                        break;
+
+                    case Key.Down:
+                    case Key.S:
+                        player.Beschleunige(false);
+                        break;
+
+                    case Key.Space:
+                        gameObjects.Add(player.Schuss());//Rückgabe: Photonentorpedo
+                        break;
+                }
+            }
         }
     }
 }
